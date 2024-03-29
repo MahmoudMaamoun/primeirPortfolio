ea20c4726fc37f220d37736f42e1e574bd07ce61
Mahmoud Maamoun <maamounsharaky@gmail.com>
2024-03-29 14:15:32 +0200

    Initial Commit

---
+++ PrimerPortfolio/PrimerPortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrimerPortfolioApp.swift
+//  PrimerPortfolio
+//
+//  Created by Mahmoud Maamoun on 29/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct PrimerPortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
